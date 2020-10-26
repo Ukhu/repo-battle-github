58eaeeacf84cf4a906e10556a690b0786b32e994
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2020-10-26 23:46:10 +0100

    Create Battle Instructions component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Popular from './components/Popular'
+// import Popular from './components/Popular'
+import Battle from './components/Battle'
 import './styles.css'
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Popular />
+        {/* <Popular /> */}
+        <Battle />
       </div>
     )
   }
