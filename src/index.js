800559f708911fb121079d29e385b50fe75d672a
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2020-11-06 21:39:42 +0100

    chore: code-splitting with react and suspense

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import Nav from './components/Nav'
-import { ThemeProvider } from './contexts/theme'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Results from './components/Results'
+import Nav from './components/Nav'
+import Loading from './components/Loading'
+import { ThemeProvider } from './contexts/theme'
 import './styles.css'
+
+const Popular = React.lazy(() => import('./components/Popular'));
+const Battle = React.lazy(() => import('./components/Battle'));
+const Results = React.lazy(() => import('./components/Results'));
 
 class App extends React.Component {
   state = {
@@ -25,17 +27,18 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="container">
               <Nav />
-
-              <Switch>
-                <Route exact path="/" component={Popular}/>
-                <Route exact path="/battle" component={Battle}/>
-                <Route path="/battle/results" component={Results}/>
-                <Route render={() => (
-                  <h1 className="center-text">
-                    404
-                  </h1>
-                )}/>
-              </Switch>
+              <React.Suspense fallback={<Loading/>}>
+                <Switch>
+                  <Route exact path="/" component={Popular}/>
+                  <Route exact path="/battle" component={Battle}/>
+                  <Route path="/battle/results" component={Results}/>
+                  <Route render={() => (
+                    <h1 className="center-text">
+                      404
+                    </h1>
+                  )}/>
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
